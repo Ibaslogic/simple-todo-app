cd648d8adf9bff774944426bdce1c88aa58f0f06
ibaslogic <ibaskunle@gmail.com>
2019-12-06 01:57:17 +0100

    Updates

---
+++ precache-manifest.31d4433e0ec69774bd7a4fd76ca808f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a9ce61cf7cd01920c889569532f47f",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "d3e70536ba783982152e",
+    "url": "/simple-todo-app/static/css/main.b1e8f748.chunk.css"
+  },
+  {
+    "revision": "26fe7dcbb2f4a8fe571f",
+    "url": "/simple-todo-app/static/js/2.c2774130.chunk.js"
+  },
+  {
+    "revision": "d3e70536ba783982152e",
+    "url": "/simple-todo-app/static/js/main.372b2428.chunk.js"
+  },
+  {
+    "revision": "44b15853b2459421772a",
+    "url": "/simple-todo-app/static/js/runtime-main.12caa410.js"
+  }
+]);
