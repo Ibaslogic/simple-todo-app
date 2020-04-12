38ee5057db474aa362f468ba5aec69092658fe9f
ibaslogic <ibaskunle@gmail.com>
2020-04-12 14:21:24 +0100

    code updated

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,58 +2,63 @@ import React from "react";
 import TodosList from "./TodosList";
 import Header from "./Header";
 import InputTodo from "./InputTodo";
-import uuid from "uuid";
+// import uuid from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component {
   state = {
     todos: [
       {
-        id: uuid.v4(),
+        // id: uuid.v4(),
+        id: uuidv4(),
         title: "Setup development environment",
-        completed: true
+        completed: true,
       },
       {
-        id: uuid.v4(),
+        // id: uuid.v4(),
+        id: uuidv4(),
         title: "Develop website and add content",
-        completed: false
+        completed: false,
       },
       {
-        id: uuid.v4(),
+        // id: uuid.v4(),
+        id: uuidv4(),
         title: "Deploy to live server",
-        completed: false
-      }
-    ]
+        completed: false,
+      },
+    ],
   };
 
-  handleChange = id => {
+  handleChange = (id) => {
     this.setState({
-      todos: this.state.todos.map(todo => {
+      todos: this.state.todos.map((todo) => {
         if (todo.id === id) {
           todo.completed = !todo.completed;
         }
         return todo;
-      })
+      }),
     });
   };
 
-  delTodo = id => {
+  delTodo = (id) => {
     this.setState({
       todos: [
-        ...this.state.todos.filter(todo => {
+        ...this.state.todos.filter((todo) => {
           return todo.id !== id;
-        })
-      ]
+        }),
+      ],
     });
   };
 
-  addTodoItem = title => {
+  addTodoItem = (title) => {
     const newTodo = {
-      id: uuid.v4(),
+      // id: uuid.v4(),
+      id: uuidv4(),
       title: title,
-      completed: false
+      completed: false,
     };
     this.setState({
-      todos: [...this.state.todos, newTodo]
+      todos: [...this.state.todos, newTodo],
     });
   };
 
